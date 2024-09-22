003cc414212c7a31f0e1483c87db688bdad0138b
vamsi0874 <vamsim0874>
2024-09-23 00:01:06 +0530

    update

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,8 +4,8 @@ import axios from 'axios';
 
 const api = axios.create({
 
-   baseURL: 'http://localhost:5000/api',
-  //  baseURL: 'https://panel-backend-g3zw.onrender.com/api',
+  //  baseURL: 'http://localhost:5000/api',
+   baseURL: 'https://panel-backend-g3zw.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
