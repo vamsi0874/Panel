9c0c189a6809f50920ec4324017cf6134b12efe3
vamsi0874 <126262877+vamsi0874@users.noreply.github.com>
2024-09-22 19:11:04 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+   baseURL: 'https://panel-backend-g3zw.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
